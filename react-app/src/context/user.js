b8b2db8707b2c8e01c38dd9b4d86dcbefb9fdd6e
RyanChehab <ryanchehab20@gmail.com>
2024-12-02 12:05:03 +0200

    js <=> fetched the api

---
+++ react-app/src/context/user.js
@@ -0,0 +1,26 @@
+import apiRoutes from "../utils/api";
+import { createContext, useState } from 'react';
+import axios from "axios";
+
+const MyContext = createContext();
+
+const ContextProvider = ({ children }) => {
+    const grab = async()=>{
+    try{
+        const response = await axios.post("http://127.0.0.1:8000/api/projects")
+
+        console.log(response.data)
+    }catch(error){
+        console.error("problem fetching the api")
+    }
+        
+    }   
+    
+    return (
+        <MyContext.Provider value={{ }}>
+            {children}
+        </MyContext.Provider>
+    );
+};
+
+export { MyContext, ContextProvider };
