5d5a326de13975b8886c112359a6542a2aee82fc
RyanChehab <ryanchehab20@gmail.com>
2024-12-02 12:16:07 +0200

    php <=> created projects method

--- laravel/app/Http/Controllers/projectsController.php
+++ laravel/app/Http/Controllers/projectsController.php
@@ -6,5 +6,12 @@ use Illuminate\Http\Request;
 
 class projectsController extends Controller
 {
-    //
+    public function projects($id){
+        $record = Project::where('id',$id)->first();
+
+        return response()->json([
+            'message'=>'retrieved successfully',
+            'data'=> $record,
+        ],201);
+    }
 }
