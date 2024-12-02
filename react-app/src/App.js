d3823d95bad44d5f59973b702a95e5d79e682f0b
RyanChehab <ryanchehab20@gmail.com>
2024-12-02 11:02:26 +0200

    js <=> imported contextProvider in app.js

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,16 +1,19 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
 import Projects from "./pages/Projects";
+import { ContextProvider } from "./context/user";
 
 function App() {
   return (
-    <div className="App">
-      <BrowserRouter>
-        <Routes>
-          <Route path="/projects" element={<Projects />} />
-        </Routes>
-      </BrowserRouter>
-    </div>
+    <ContextProvider>
+      <div className="App">
+        <BrowserRouter>
+          <Routes>
+            <Route path="/projects" element={<Projects />} />
+          </Routes>
+        </BrowserRouter>
+      </div>
+    </ContextProvider>
   );
 }
 
