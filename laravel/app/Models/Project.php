5f140b0758caa2d7b345cb180bdf24049ca5ce88
RyanChehab <ryanchehab20@gmail.com>
2024-12-02 11:58:43 +0200

    php <=> created project model

---
+++ laravel/app/Models/Project.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Project extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [];
+    
+    public function users(){
+        return $this->belongsToMany(User::class,'project_user','project_id','user_id');
+    }
+}
